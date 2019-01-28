39746424b0599e03f32806ae6bf8e15c39cf82ea
rogerchao <shuang_jui_chao@hotmail.com>
2019-01-28 22:05:27 +0800

    test

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=rogerchao-echo-bot-Web-Deploy.pubxml -p:Password=ihTgw96ag9eqaSAvplwjme6mhTT3xi16DsCZvMnaikfmgPsM0dtmplSpL8xf
+
