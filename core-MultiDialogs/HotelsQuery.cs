906d47af95b006f7b3cfa303973b4800e16aec74
rogerchao <shuang_jui_chao@hotmail.com>
2019-01-28 22:36:00 +0800

    MultiDialogsBot Sample

---
+++ core-MultiDialogs/HotelsQuery.cs
@@ -0,0 +1,20 @@
+﻿namespace MultiDialogsBot
+{
+    using System;
+    using Microsoft.Bot.Builder.FormFlow;
+
+    [Serializable]
+    public class HotelsQuery
+    {
+        //6.1
+        [Prompt("Please enter your {&}")]
+        public string Destination { get; set; }
+
+        [Prompt("When do you want to {&}?")]
+        public DateTime CheckIn { get; set; }
+
+        [Numeric(1, int.MaxValue)]
+        [Prompt("How many {&} do you want to stay?")]
+        public int Nights { get; set; }
+    }
+}
