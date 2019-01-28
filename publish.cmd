58a6fc4ba05048ed071e1bed234cd05974627ce3
rogerchao <shuang_jui_chao@hotmail.com>
2019-01-28 22:06:48 +0800

    delete

--- publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=rogerchao-echo-bot-Web-Deploy.pubxml -p:Password=ihTgw96ag9eqaSAvplwjme6mhTT3xi16DsCZvMnaikfmgPsM0dtmplSpL8xf
-
