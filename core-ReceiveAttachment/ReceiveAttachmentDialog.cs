6a6a157137e5e34d0ab49215eac63da7f40c0b2e
rogerchao <shuang_jui_chao@hotmail.com>
2019-01-28 22:49:02 +0800

    add core-ReceiveAttachment

---
+++ core-ReceiveAttachment/ReceiveAttachmentDialog.cs
@@ -0,0 +1,40 @@
+﻿namespace ReceiveAttachmentBot
+{
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
+
+    [Serializable]
+    internal class ReceiveAttachmentDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(this.MessageReceivedAsync);
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> argument)
+        {
+            var message = await argument;
+
+            if (message.Attachments != null && message.Attachments.Any())
+            {
+                var attachment = message.Attachments.First();
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    //8.1
+                }
+            }
+            else
+            {
+                await context.PostAsync("Hi there! I'm a bot created to show you how I can receive message attachments, but no attachment was sent to me. Please, try again sending a new message including an attachment.");
+            }
+
+            context.Wait(this.MessageReceivedAsync);
+        }
+    }
+}
